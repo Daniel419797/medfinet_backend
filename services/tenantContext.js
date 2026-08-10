41d75b737a572af2bf37e3991de399d844d9f2ad
Danny <120275321+Daniel419797@users.noreply.github.com>
2026-08-10 18:04:24 +0100

    Support explicit tenant transaction isolation

--- services/tenantContext.js
+++ services/tenantContext.js
@@ -1,17 +1,29 @@
 const { DomainError } = require('../utils/domainError');
 
-async function withTenantTransaction(prismaClient, organizationId, operation) {
+async function withTenantTransaction(
+  prismaClient,
+  organizationId,
+  operation,
+  options = {}
+) {
   if (!organizationId || typeof organizationId !== 'string') {
     throw new DomainError(400, 'ORGANIZATION_REQUIRED', 'An organization context is required');
   }
 
-  return prismaClient.$transaction(async (transaction) => {
+  const run = async (transaction) => {
     await transaction.$executeRawUnsafe(
       "SELECT set_config('app.current_organization_id', $1, true)",
       organizationId
     );
     return operation(transaction);
-  });
+  };
+
+  if (options.isolationLevel) {
+    return prismaClient.$transaction(run, {
+      isolationLevel: options.isolationLevel,
+    });
+  }
+  return prismaClient.$transaction(run);
 }
 
 module.exports = { withTenantTransaction };
